118033a1e09a055965b359be4e0bb1c835770fce
Ravi <ravi.pr90s@gmail.com>
2023-12-29 02:42:25 +0530

    banner update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  const user = false;
+  // const user = false;
   return (
     <>
       <BrowserRouter>
