5c60d23bbb40f469ddda3a63ade106f77b590e09
Ravi <ravi.pr90s@gmail.com>
2023-12-29 03:08:12 +0530

    redux update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  // const user = false;
+  const user = false;
   return (
     <>
       <BrowserRouter>
