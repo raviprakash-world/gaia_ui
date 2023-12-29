7592f4de30f6f63b59b3e0d181b315ce28833e33
Ravi <ravi.pr90s@gmail.com>
2023-12-29 11:18:18 +0530

    Routes added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const MainContainer = styled.div`
+  background-color: whitesmoke;
+`;
+
+const Home = () => {
+  return (
+    <div>
+      <MainContainer>
+        HOME
+      </MainContainer>
+      
+    </div>
+  );
+};
+
+export default Home;
