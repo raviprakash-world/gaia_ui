7a24dd77a97830ada2d114eeba1a8ae1cc21f9a3
Ravi <ravi.pr90s@gmail.com>
2023-12-29 12:50:55 +0530

    Footer added on Home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import UpperAnnouncement from "../components/UpperAnnouncement.jsx";
 import Navbar from "../components/Navbar.jsx";
+import Footer from "../components/Footer.jsx";
 
 
 const MainContainer = styled.div`
@@ -14,6 +15,7 @@ const Home = () => {
       <MainContainer>
         <UpperAnnouncement />
         <Navbar/>
+        <Footer/>
       </MainContainer>
       
     </div>
